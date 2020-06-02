8220a2cf10500ea5e00676585391ea9fc60520db
surajgurung <surajgurung@192.168.0.13>
2020-06-01 21:06:54 -0500

    initial commit

---
+++ src/test/java/B1300/B1300/AppTest.java
@@ -0,0 +1,20 @@
+package B1300.B1300;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
